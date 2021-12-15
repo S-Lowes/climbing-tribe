1438a906012bd4f4ffa487c4feaff158e9bbc3a5
Sebastian <seblowes@btinternet.com>
2021-12-15 19:29:32 +0000

    (feat): Added Favicon

--- billy/urls.py
+++ billy/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(
+        url=staticfiles_storage.url('favicon/favicon.ico'))),
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
